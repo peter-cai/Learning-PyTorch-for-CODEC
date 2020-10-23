9332d40c21cf9fe545efd12c55dff65614e0733b
peter-cai <991480587@qq.com>
2020-10-23 17:17:17 +0800

    20201023

--- autograd.py
+++ autograd.py
@@ -1,49 +1,49 @@
-# -*- coding: utf-8 -*-
+# -*- coding=utf-8 -*-
 import torch
 
 dtype = torch.float
 device = torch.device("cpu")
-# device = torch.device（“cuda：0”）＃取消注释以在GPU上运行
+# device = torch.device（“cuda：0”）＃ uncomment to use gpu
 
-# N是批量大小; D_in是输入维度;
-# H是隐藏的维度; D_out是输出维度。
+# N is the numbers of input and D_in is the input size
+# H is the size of hidden layer; D_out is the output size
 N, D_in, H, D_out = 64, 1000, 100, 10
 
-# 创建随机Tensors以保持输入和输出。
-# 设置requires_grad = False表示我们不需要计算渐变
-# 在向后传球期间对于这些Tensors。
+# create the random input and output following the normal distribution
 x = torch.randn(N, D_in, device=device, dtype=dtype)
 y = torch.randn(N, D_out, device=device, dtype=dtype)
 
-# 为权重创建随机Tensors。
-# 设置requires_grad = True表示我们想要计算渐变
-# 在向后传球期间尊重这些张贴。
+# create the random weight and bias following the normal distribution
+# set requires_grad = True, so the gradient can be obtained in related parameters
 w1 = torch.randn(D_in, H, device=device, dtype=dtype, requires_grad=True)
 w2 = torch.randn(H, D_out, device=device, dtype=dtype, requires_grad=True)
 
+# set the learning rate, which can control the scale of parameter change
 learning_rate = 1e-6
 for t in range(500):
-    # 前向传播：使用tensors上的操作计算预测值y; 
-      # 由于w1和w2有requires_grad=True，涉及这些张量的操作将让PyTorch构建计算图，
-    # 从而允许自动计算梯度。由于我们不再手工实现反向传播，所以不需要保留中间值的引用。
+    # forward propogation and get the prediction values of y 
+    # the computing graph is formed becasue the requires_grad is set as True
+    # the gradient is calculated automately, which is one of the important tools in PyTorch
+    # .mm() is to calculate the matrix multiplication
+    # torch.clamp(input, min, max, out=None) is to control the limit of tensor
+    # torch.clamp is similar to ReLU（Rectified Linear Unit） function
     y_pred = x.mm(w1).clamp(min=0).mm(w2)
 
-    # 使用Tensors上的操作计算和打印丢失。
-    # loss是一个形状为()的张量
-    # loss.item() 得到这个张量对应的python数值
+    # calculate the loss by MSE (mean square error)
+    # loss is also a tensor and it can be changed into Scale by the function of .item()
     loss = (y_pred - y).pow(2).sum()
     print(t, loss.item())
 
-    # 使用autograd计算反向传播。这个调用将计算loss对所有requires_grad=True的tensor的梯度。
-    # 这次调用后，w1.grad和w2.grad将分别是loss对w1和w2的梯度张量。
+    # use autograd to calculate the backward propogation 
+    # all the gradients of tensor with requires_grad=True can be got
     loss.backward()
 
-    # 使用梯度下降更新权重。对于这一步，我们只想对w1和w2的值进行原地改变；不想为更新阶段构建计算图，
-    # 所以我们使用torch.no_grad()上下文管理器防止PyTorch为更新构建计算图
+    # torch.no_grad(): update the weight and not change the computing graph
+    # .grad() to get the gradient
     with torch.no_grad():
         w1 -= learning_rate * w1.grad
         w2 -= learning_rate * w2.grad
 
-        # 反向传播后手动将梯度设置为零
+        # clear the gradients each step when it has been used, otherwise the gradients will be accumlated
         w1.grad.zero_()
         w2.grad.zero_()
